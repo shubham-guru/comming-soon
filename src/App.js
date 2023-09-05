7b1d60b13a709afb755837cc05c8129ade349f87
shubham-guru <gurushubham4@gmail.com>
2023-09-05 20:36:59 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import logo from "./logo.png"
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <img src={logo} alt="logo" width="400vmin"  />
+        <h3 className="text">Comming Soon...</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
